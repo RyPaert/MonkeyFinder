b758d83caee532a15010758f12185af6004c27dd
Opilane TTHK <158561974+RyPaert@users.noreply.github.com>
2025-05-05 16:48:11 +0300

    2:37:37

--- MonkeyFinder/View/DetailsPage.xaml.cs
+++ MonkeyFinder/View/DetailsPage.xaml.cs
@@ -1,7 +1,9 @@
 namespace MonkeyFinder;
 
+[QueryProperty("Monkey", "Monkey")]
 public partial class DetailsPage : ContentPage
 {
+	public Monkey Monkey { get; set; }
 	public DetailsPage()
 	{
 		InitializeComponent();
