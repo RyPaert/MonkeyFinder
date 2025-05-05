2c601ad2ebca5f13ecb576790164a5597140a6bb
Opilane TTHK <158561974+RyPaert@users.noreply.github.com>
2025-05-05 16:51:48 +0300

    2:40:49

--- MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,5 +1,9 @@
 ﻿namespace MonkeyFinder.ViewModel;
-
+[QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
+    public MonkeyDetailsViewModel()
+    {
+
+    }
 }
